fb449ea9acf754697296c273fd5fad366cfecb52
mano jyoti <manojyoti434@gmail.com>
2025-07-20 16:33:55 +0530

    referencing vpc module test and peering also

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 module "vpc" {
-    source = "../terraform-aws-vpc"
+    # source = "../terraform-aws-vpc"
     /* project = "roboshop"
     environment = "dev"
     public_subnet_cidrs = var.public_subnet_cidrs */
-
+    source = "git::https://github.com/manojyoti434/terraform-aws-vpc.git?ref=main"
     project= var.project
     environment = var.environment
     public_subnet_cidrs = var.private_subnet_cidrs
