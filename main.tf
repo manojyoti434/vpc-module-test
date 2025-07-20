d47ef8599105cc1ca55d8450bbcc3f03adf9cda8
mano jyoti <manojyoti434@gmail.com>
2025-07-20 16:37:52 +0530

    referencing vpc module test and peering also

--- main.tf
+++ main.tf
@@ -4,6 +4,9 @@ module "vpc" {
     environment = "dev"
     public_subnet_cidrs = var.public_subnet_cidrs */
     source = "git::https://github.com/manojyoti434/terraform-aws-vpc.git?ref=main"
+
+    # above line is calling module  process will be done in practical, if we run reconfigure and plan then it will stores all module files into this folder for reference# 
+    # bucket should be created   manually before doing it #
     project= var.project
     environment = var.environment
     public_subnet_cidrs = var.private_subnet_cidrs
