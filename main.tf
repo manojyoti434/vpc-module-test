9b7471eaba82393ba536543c75c25aca2e42d170
mano jyoti <manojyoti434@gmail.com>
2025-07-20 16:41:30 +0530

    referencing vpc module test and peering also

--- main.tf
+++ main.tf
@@ -5,8 +5,9 @@ module "vpc" {
     public_subnet_cidrs = var.public_subnet_cidrs */
     source = "git::https://github.com/manojyoti434/terraform-aws-vpc.git?ref=main"
 
-    # above line is calling module  process will be done in practical, if we run reconfigure and plan then it will stores all module files into this folder for reference# 
+    # above line is calling module  process will be done in practical, if we run terraform init reconfigure and plan then it will downloads and stores all module files into this folder for reference# 
     # bucket should be created   manually before doing it #
+    # if modules in original folder got updated by module developer then we, module users need to run this cmd, terraform init upgrade thenall new changes again get download and stored in this repo as weell # 
     project= var.project
     environment = var.environment
     public_subnet_cidrs = var.private_subnet_cidrs
